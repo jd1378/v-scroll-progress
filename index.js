21a2769c60dab4821d6da34c3f4a43f3cd1d9451
Javad Mnjd <javad.mnjd@hotmail.com>
2022-07-01 01:25:22 +0430

    compatibility update for vue 3

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
+const inserted = (el, binding) => {
+  if (typeof binding.value !== "function") {
+    return;
+  }
+
+  const isHorizontal = !!binding.modifiers.horizontal;
+  const f = function (e) {
+    let progress;
+    if (isHorizontal) {
+      const currentScroll = el.scrollLeft;
+      const width = el.scrollWidth - el.clientWidth;
+      progress = (currentScroll / width) * 100;
+    } else {
+      const currentScroll = el.scrollTop;
+      const height = el.scrollHeight - el.clientHeight;
+      progress = (currentScroll / height) * 100;
+    }
+    if (isNaN(progress)) {
+      return;
+    }
+    binding.value(progress);
+  };
+
+  el._scrollProgress = f;
+
+  el.addEventListener("scroll", f, {
+    passive: true,
+  });
+};
+
+const unbind = (el, binding) => {
+  if (el._scrollProgress) {
+    el.removeEventListener("scroll", el._scrollProgress);
+  }
+};
 
 const plugin = {
   install(Vue, options) {
-
-    Vue.directive('scroll-progress', {
-      inserted(el, binding) {
-        if (typeof binding.value !== 'function') {
-          return;
-        }
-
-        const isHorizontal = !!binding.modifiers.horizontal;
-        const f = function(e) {
-          let progress;
-          if (isHorizontal) {
-            const currentScroll = el.scrollLeft;
-            const width = el.scrollWidth - el.clientWidth;
-            progress = (currentScroll / width) * 100;
-          } else {
-            const currentScroll = el.scrollTop;
-            const height = el.scrollHeight - el.clientHeight;
-            progress = (currentScroll / height) * 100;
-          }
-          if (isNaN(progress)) {
-            return;
-          }
-          binding.value(progress);
-        };
-
-        el._scrollProgress = f;
-
-        el.addEventListener(
-          'scroll',
-          f,
-          {
-            passive: true
-          }
-        );
-      },
-      unbind(el, binding) {
-        if (el._scrollProgress) {
-          el.removeEventListener('scroll', el._scrollProgress);
-        }
-      }
+    Vue.directive("scroll-progress", {
+      inserted,
+      mounted: inserted, // vue 3 "inserted"
+      unbind,
+      unmounted: unbind, // vue 3 "unbind"
     });
-
-  }
+  },
 };
 
 export default plugin;
